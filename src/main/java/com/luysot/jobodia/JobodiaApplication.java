8b293c0d86d0432206a55741c86a8120ca253107
Ranoscyber <sonayaya92@gmail.com>
2026-06-10 17:43:06 +0700

    test : test pull request with telegram bot

--- src/main/java/com/luysot/jobodia/JobodiaApplication.java
+++ src/main/java/com/luysot/jobodia/JobodiaApplication.java
@@ -10,7 +10,7 @@ public class JobodiaApplication {
 
         SpringApplication.run(JobodiaApplication.class, args);
 
-        System.out.println("The Jobodia Backend Started...");
+        System.out.println("The Jobodiaaaa Backend Started...");
 	}
 
 }
