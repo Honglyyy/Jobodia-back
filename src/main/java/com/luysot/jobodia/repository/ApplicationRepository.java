af33d2aad418684433ec9d06802d1de84d088017
Lay Hongly <honglylay14@gmail.com>
2026-07-04 14:51:49 +0700

    -fix: extract the finding of user to a method in applications -feat: seeker can fetch their applications and application. employer can fetch the applicant that apply for the job from their company -feat: seeker can delete the application that they have applied -feat: employer can update the status of application

--- src/main/java/com/luysot/jobodia/repository/ApplicationRepository.java
+++ src/main/java/com/luysot/jobodia/repository/ApplicationRepository.java
@@ -1,7 +1,19 @@
 package com.luysot.jobodia.repository;
 
 import com.luysot.jobodia.model.Applications;
+import com.luysot.jobodia.model.EmployerProfiles;
+import com.luysot.jobodia.model.SeekerProfiles;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ApplicationRepository extends JpaRepository<Applications, Long> {
+    List<Applications> findBySeeker(SeekerProfiles seeker);
+    Optional<Applications> findByIdAndSeeker(Long id, SeekerProfiles seeker);
+    void deleteByIdAndSeeker(Long id, SeekerProfiles seeker);
+    List<Applications> findByJobEmployerId(Long employerId);
+
+    Optional<Applications> findByJobEmployerId_AndId(Long jobEmployerId, Long id);
 }
--- src/main/java/com/luysot/jobodia/service/ApplicationService.java
+++ src/main/java/com/luysot/jobodia/service/ApplicationService.java
@@ -2,14 +2,18 @@ package com.luysot.jobodia.service;
 
 import com.luysot.jobodia.dto.ApplicationDTOs.ApplicationRequestDto;
 import com.luysot.jobodia.dto.ApplicationDTOs.ApplicationResponseDto;
+import com.luysot.jobodia.dto.ApplicationDTOs.UpdateApplicationStatusRequestDto;
 import com.luysot.jobodia.mapper.ApplicationMapper;
 import com.luysot.jobodia.model.*;
 import com.luysot.jobodia.repository.*;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,13 +25,11 @@ public class ApplicationService {
     private final SeekerResumeRepository seekerResumeRepository;
     private final SeekerCoverLetterRepository seekerCoverLetterRepository;
     private final ApplicationMapper applicationMapper;
+    private final EmployerProfileRepository employerProfileRepository;
 
     @Transactional
     public ApplicationResponseDto apply(String email, ApplicationRequestDto dto){
-        Users user = userRepository.findByEmail(email)
-                .orElseThrow(()-> new UsernameNotFoundException("User not found."));
-        SeekerProfiles seeker = seekerProfileRepository.findByUser(user)
-                .orElseThrow(()-> new UsernameNotFoundException("User not found."));
+        SeekerProfiles seeker = getSeekerProfiles(email);
         SeekerResumes resume = seekerResumeRepository.findByIdAndSeeker(dto.resumeId(), seeker)
                 .orElseThrow(()-> new RuntimeException("Resume not found"));
         SeekerCoverLetters coverLetter = seekerCoverLetterRepository.findByIdAndSeeker(dto.coverLetterId(),seeker)
@@ -44,5 +46,78 @@ public class ApplicationService {
         Applications savedApplication = applicationRepository.save(application);
 
         return applicationMapper.toDto(savedApplication);
+    }
+
+    public List<ApplicationResponseDto> findSeekerOwnApplications(String email){
+        SeekerProfiles seeker = getSeekerProfiles(email);
+
+        List<Applications> applications = applicationRepository.findBySeeker(seeker);
+
+        return applications.stream()
+                .map(applicationMapper::toDto)
+                .toList();
+    }
+
+    public ApplicationResponseDto findSeekerOwnApplication(Long id, String email){
+        SeekerProfiles seeker = getSeekerProfiles(email);
+
+        Applications applications = applicationRepository.findByIdAndSeeker(id,seeker)
+                .orElseThrow(()->new RuntimeException("Application not found"));
+
+        return applicationMapper.toDto(applications);
+    }
+
+    @Transactional
+    public void deleteSeekerOwnApplication(Long id, String email){
+        SeekerProfiles seeker = getSeekerProfiles(email);
+
+        applicationRepository.deleteByIdAndSeeker(id,seeker);
+    }
+
+    public List<ApplicationResponseDto> findApplicants(String email){
+        EmployerProfiles employer = getEmployerProfiles(email);
+
+        List<Applications> applications = applicationRepository.findByJobEmployerId(employer.getId());
+
+        return applications.stream()
+                .map(applicationMapper::toDto)
+                .toList();
+    }
+
+    public ApplicationResponseDto findApplicant(Long applicationId,String email){
+        EmployerProfiles employer = getEmployerProfiles(email);
+
+        Applications application = applicationRepository.findByJobEmployerId_AndId(employer.getId(),applicationId)
+                .orElseThrow(()->new RuntimeException("Applicant not found"));
+
+        return applicationMapper.toDto(application);
+    }
+
+
+    public ApplicationResponseDto updateApplicationStatus( Long applicationId, UpdateApplicationStatusRequestDto reqStatus,String email){
+        EmployerProfiles employer = getEmployerProfiles(email);
+
+        Applications application = applicationRepository.findByJobEmployerId_AndId(employer.getId(),applicationId)
+                .orElseThrow(()->new RuntimeException("Applicant not found"));
+
+        application.setStatus(reqStatus.status());
+        applicationRepository.save(application);
+
+        return applicationMapper.toDto(application);
+    }
+
+
+    private @NonNull SeekerProfiles getSeekerProfiles(String email) {
+        Users user = userRepository.findByEmail(email)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found."));
+        return seekerProfileRepository.findByUser(user)
+                .orElseThrow(()-> new UsernameNotFoundException("Seeker with the following email is not found."));
+    }
+
+    private @NonNull EmployerProfiles getEmployerProfiles(String email) {
+        Users user = userRepository.findByEmail(email)
+                .orElseThrow(()-> new UsernameNotFoundException("User not found."));
+        return employerProfileRepository.findByUser(user)
+                .orElseThrow(()-> new UsernameNotFoundException("Employer with the following email is not found."));
     }
 }
